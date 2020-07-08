b89f1e99abf018bc1d3cc26fd801def11f153155
Саша Воробьёв <avorobiev@deloitte.ru>
2020-07-08 09:36:34 +0300

    Поменял иконку

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./public/frontend")))
+
+	if checkExists("cert.pem") && checkExists("key.pem") {
+		fmt.Println("SSL web server up")
+		http.ListenAndServeTLS(":10443", "cert.pem", "key.pem", nil)
+	} else {
+		fmt.Println("Plain web server up")
+		http.ListenAndServe(":8080", nil)
+	}
+}
+
+func checkExists(filename string) bool {
+
+	if _, err := os.Stat(filename); err == nil {
+		return true
+	}
+
+	return false
 }
